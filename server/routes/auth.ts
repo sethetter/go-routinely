9e034a94102bfad535411acd607024a2c1ba90a9
Seth Etter <sethetter@gmail.com>
2018-05-11 16:22:43 -0500

    Auth0 w/ Passport working now

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,38 +1,40 @@
 import * as express from 'express'
+import * as passport from 'passport'
+import * as Auth0Strategy from 'passport-auth0'
 
 const router = express.Router()
 
-router.get('/login', (req, res) => {
-  // TODO: this
+passport.use(new Auth0Strategy({
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL: '/auth/callback'
+// @ts-ignore
+}, (_accessToken, _refreshToken, _extraParams, profile, done) => {
+  return done(undefined, profile)
+}))
+
+passport.serializeUser((user, done) => done(undefined, user))
+passport.deserializeUser((user, done) => done(undefined, user))
+
+// Start the auth flow, send user over to Auth0
+router.get('/login', passport.authenticate('auth0', {}), (_req, res) => {
   res.redirect('/')
 })
 
 router.get('/logout', (req, res) => {
   req.logout()
-
-  res.clearCookie('_routinely_token', { path: '' })
-  res.clearCookie('_routinely_user', { path: '' })
-
-  res.redirect('/')
+  return res.redirect('/')
 })
 
-// router.get('/callback', passport.authenticate('auth0', {
-//   failureRedirect: '/login'
-// }), (req, res) => {
-//   if (!req.user) throw new Error('No user!')
-
-//   // TODO: confirm options for best security
-//   res.cookie('_routinely_token', req.user.extraParams.id_token, {
-//     path: '',
-//     maxAge: req.user.extraParams.expires_in
-//   })
-
-//   res.cookie('_routinely_user', req.user.profile.id, {
-//     path: '',
-//     maxAge: req.user.extraParams.expires_in
-//   })
-
-//   res.redirect('/')
-// })
+// Receives a successful login session from Auth0, sets the access token in the session
+router.get('/callback', passport.authenticate('auth0', {
+  failureRedirect: '/'
+}), (req, res) => {
+  if (!req.user) {
+    throw new Error('No user found!')
+  }
+  return res.redirect('/')
+})
 
 export default router
--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,22 @@
 import * as path from 'path'
 import * as dotenv from 'dotenv'
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 dotenv.config({ path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`) })
 
 import * as express from 'express'
 import * as http from 'http'
 import * as bodyParser from 'body-parser'
 import * as cookieParser from 'cookie-parser'
+import * as session from 'express-session'
+import * as mongoose from 'mongoose'
+import * as createMongoStore from 'connect-mongo'
+import * as passport from 'passport'
+import { ensureLoggedIn } from 'connect-ensure-login'
 
-// import authRoutes from './routes/auth'
-import userRoutes from './routes/user'
+import authRoutes from './routes/auth'
+
+const MongoStore = createMongoStore(session)
 
 export type NextHandler = (
   req: http.IncomingMessage,
@@ -18,19 +25,46 @@ export type NextHandler = (
 
 export default (nextHandler: NextHandler): express.Application => {
   const app = express()
-  const router = require('express-promise-router')()
+  
+  mongoose.connect(process.env.MONGO_URL)
 
+  app.use(cookieParser(process.env.SESSION_SECRET || 'shwat'))
   app.use(bodyParser.json())
-  app.use(cookieParser())
-  app.use(router)
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(session({
+    secret: process.env.SESSION_SECRET || 'shwat',
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    }),
+    cookie: {
+      path: '/',
+      secure: process.env.NODE_ENV === 'production'
+    },
+    resave: false,
+    saveUninitialized: false
+  }))
 
-  // app.use('/api/auth', authRoutes)
-  app.use('/api/user', userRoutes)
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  // TODO: Is this necessary with asyncHandler? Is it a TS issue?
+  const asyncRouter = require('express-promise-router')()
+  app.use(asyncRouter)
+
+  app.use('/auth', authRoutes)
+  app.get('/api/stuff', ensureLoggedIn('/auth/login'), (req, res) => {
+    return res.json(req.user)
+  })
 
   const errorHandler: express.ErrorRequestHandler =
     (err, _req, res, next) => {
       if (!err) return next()
-      return res.status(err.status).json(err.expose ? err : {})
+      console.error(err)
+
+      const status = err.status || 500
+      return res.status(status).json(
+        err.expose || process.env.NODE_ENV === 'development' ? err : {}
+      )
     }
 
   app.use(errorHandler)
