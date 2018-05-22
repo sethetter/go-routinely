1ef279c182abee049c7c23f9d27de96f82c6dff8
Seth Etter <sethetter@gmail.com>
2018-05-22 05:26:05 -0500

    Switch back to only using port 3000

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import * as next from 'next'
 import server from './server'
 
 const dev = process.env.NODE_ENV !== 'production'
-const port = parseInt(process.env.PORT, 10) || dev ? 3000 : 443
+const port = 3000
 
 const nextApp = next({ dev })
 const nextHandler = nextApp.getRequestHandler()
