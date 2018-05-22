981b9a88a894f7dc96bd01aa59e3a7dc6a2a98f7
Seth Etter <sethetter@gmail.com>
2018-05-22 04:59:57 -0500

    Production default port to 443

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,8 @@
 import * as next from 'next'
 import server from './server'
 
-const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
+const port = parseInt(process.env.PORT, 10) || dev ? 3000 : 443
 
 const nextApp = next({ dev })
 const nextHandler = nextApp.getRequestHandler()
@@ -10,6 +10,6 @@ const nextHandler = nextApp.getRequestHandler()
 nextApp.prepare().then(() => {
   server(nextHandler).listen(port, (err: Error) => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+    console.log(`> Ready on localhost:${port}`)
   })
 })
